6f1c409bf4fac29da5a45e16ed90e2c9eb69eeba
Austin Scott <austinscott4@gmail.com>
2019-12-04 23:37:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,10 +14,10 @@ def find_max_value(array)
   max
 end
 
-def find_min_value(array)
-  min = nil
+def find_max_value(array)
+  min = 0
   array.length.times do |count|
-    if (array[count] < min) || min == nil
+    if array[count] < min
       min = array[count]
     end
   end
